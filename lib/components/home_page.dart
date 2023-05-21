5e9ff6fdcb2332bc8ef3725150ec4349061c2a7e
alexacolbe <ibegbulam.ua@gmail.com>
2023-05-22 00:49:21 +0100

    added scroll  view to make hp scroolable

--- lib/components/home_page.dart
+++ lib/components/home_page.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'home_page/body.dart';
 
 class HomePage extends StatelessWidget {
@@ -22,10 +22,32 @@ class HomePage extends StatelessWidget {
         ],
       ),
       body: SafeArea(
-        child: Column(
-          children: const [
-            Body(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Body(),
+              SizedBox(height: 27.h),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 16.w),
+                child: SizedBox(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Text(
+                          'NEW ARRIVAL',
+                          style: TextStyle(
+                              fontSize: 18.sp,
+                              fontWeight: FontWeight.w400,
+                              letterSpacing: 4),
+                        ),
+                        SizedBox(child: Image.asset('images/3.png')),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
